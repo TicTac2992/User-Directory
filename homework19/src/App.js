cbb11e74749b81df09425d8c66a8fa333670a3dc
Jared Varnum <jvarnum3@gmail.com>
2020-10-10 04:43:03 -0500

    adding components.js files, adding utils/home and api.js, npm start - errors, npm i axios

--- homework19/src/App.js
+++ homework19/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Home from './utils/Home';
 import Header from './components/Header';
 import Container from "./components/Container";
